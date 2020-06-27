5932cb522a35d06c6096546b982752bacff584d6
java0088 <965002107@qq.com>
2020-06-27 23:04:06 +0800

    难度调整完成

--- src/views/Adjustment.vue
+++ src/views/Adjustment.vue
@@ -1,16 +1,139 @@
 <template>
   <div class="adjustment">
-       <!-- 头部导航 -->
-      <van-nav-bar title="难度调整"  left-arrow @click-left="$router.back()"/>
+    <!-- 头部导航 -->
+    <van-nav-bar title="难度调整" left-arrow @click-left="$router.back()" />
+    <div class="swipe_box">
+      <van-swipe class="my-swipe" indicator-color="white">
+        <van-swipe-item><img src="../assets/images/user2.jpg" alt=""/></van-swipe-item>
+        <van-swipe-item><img src="../assets/images/user2.jpg" alt=""/></van-swipe-item>
+        <van-swipe-item><img src="../assets/images/user2.jpg" alt=""/></van-swipe-item>
+        <van-swipe-item><img src="../assets/images/user2.jpg" alt=""/></van-swipe-item>
+      </van-swipe>
+      <!-- 级别 -->
+      <div class="jibie">
+        <div class="item">
+          <span>简单</span>
+        </div>
+      </div>
+      <div class="kaoshi">
+        <span>ETS4 A1</span>
+      </div>
+    </div>
+
+    <!-- 适合 -->
+    <div class="fit">
+      <div class="tit">适合于:</div>
+      <div class="list">
+        <p>1.有一定英语基础,希望提升的中同学</p>
+        <p>2.有一定英语基础,希望提升的中同学</p>
+        <p>3.有一定英语基础,希望提升的中同学</p>
+      </div>
+    </div>
+    <p class="done"><span class="tit">学完本级:</span> 进行日常对话,描述经历,表达简单的观点</p>
+    <!-- 按钮 -->
+    <div class="btn">
+      选择本级别
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {}
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.swipe_box {
+  width: 100%;
+  padding: 10px 0;
+  background-color: #f9f9f9;
+  height: 380px;
+  img {
+    width: 100%;
+    height: 250px;
+  }
+  .jibie {
+    position: relative;
+    margin-top: 10px;
+    width: 100%;
+    height: 4px;
+    background-color: #e5481f;
+    &::before,
+    &::after {
+      top: -3px;
+      //   transform: translate(-50%);
+      position: absolute;
+      content: '';
+      width: 12px;
+      height: 12px;
+      background-color: #e5481f;
+      border-radius: 50%;
+    }
+    &::after {
+      right: 0;
+    }
+    .item {
+      position: relative;
+      padding-top: 16px;
+      text-align: center;
+      &::before {
+        top: -3px;
+        left: 50%;
+        transform: translate(-50%);
+        position: absolute;
+        content: '';
+        width: 12px;
+        height: 12px;
+        background-color: #e5481f;
+        border-radius: 50%;
+      }
+      position: relative;
+    }
+  }
+  .kaoshi {
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+    span {
+      display: inline-block;
+      background-color: #ffbc20;
+      color: #fff;
+      border-radius: 5px;
+      padding: 6px 10px;
+    }
+  }
+}
+.btn {
+  position: fixed;
+  bottom: 16px;
+  left: 50%;
+  transform: translate(-50%);
+  width: 88%;
+  height: 42px;
+  text-align: center;
+  line-height: 42px;
+  color: #fff;
+  border-radius: 20px;
+  background-color: #e64626;
+}
+.fit {
+  display: flex;
+  padding: 12px;
+  .list {
+    margin-left: 20px;
+  }
+  p {
+    color: #646464;
+    margin-bottom: 8px;
+    font-size: 14px;
+  }
+}
+.done {
+  padding: 12px;
+  color: #646464;
+  font-size: 14px;
+}
 </style>
--- src/views/Goal.vue
+++ src/views/Goal.vue
@@ -1,55 +1,105 @@
 <template>
   <div class="goal">
-       <!-- 头部导航 -->
-      <van-nav-bar title="目标与数据"  left-arrow @click-left="$router.back()"/>
+    <!-- 头部导航 -->
+    <van-nav-bar title="目标与数据" :fixed="true" left-arrow @click-left="$router.back()" />
 
-      <!-- 内容 -->
-      <div class="content">
-        <div class="top">
-          <div class="flex_bea">
-           <div class="user_icon flex_align">
-             <div class="icon_box flex_center">
-                <span class="iconfont icon-user"></span>
-              </div>
-              <div class="user_name tit">小通</div>
+    <!-- 内容 -->
+    <div class="content">
+      <div class="top">
+        <div class="flex_bea">
+          <div class="user_icon flex_align">
+            <div class="icon_box flex_center">
+              <span class="iconfont icon-user"></span>
             </div>
-            <div class="text">学习记录</div>
-        </div>
-          <div class="study flex_align">
-            <div class="today">
-              <p>8<span class="text">/分钟</span></p>
-              <span>今日学习</span>
-            </div>
-            <div class="today">
-              <p>123<span class="text">分钟</span></p>
-              <span>总学习时长</span>
-            </div>
+            <div class="user_name tit">小通</div>
           </div>
+          <div class="text">学习记录</div>
         </div>
-
-        <!-- 当前级别 -->
-        <div class="current">
-          <div class="title">当前级别: 简单</div>
-          <div class="item">
-            <p>在本级别获取听力经验值达到320000</p>
-            <van-progress :percentage="50" track-color="#fceaea" />
+        <div class="study flex_align">
+          <div class="today">
+            <p>
+              8
+              <span class="text">/分钟</span>
+            </p>
+            <span>今日学习</span>
           </div>
-           <div class="item">
-            <p>在本级别进行各项口语练习累计300次</p>
-            <van-progress :percentage="50" />
+          <div class="today">
+            <p>
+              123
+              <span class="text">分钟</span>
+            </p>
+            <span>总学习时长</span>
           </div>
         </div>
       </div>
+
+      <!-- 当前级别 -->
+      <div class="current">
+        <div class="title">当前级别: 简单</div>
+        <div class="item">
+          <p>在本级别获取听力经验值达到320000</p>
+          <van-progress
+            :percentage="50"
+            text-color="#222"
+            color="#fb7e68"
+            stroke-width="20"
+            track-color="#fceaea"
+          />
+        </div>
+        <div class="item">
+          <p>在本级别进行各项口语练习累计300次</p>
+          <van-progress
+            :percentage="50"
+            text-color="#222"
+            color="#f77323"
+            stroke-width="20"
+            track-color="#f7f0dc"
+          />
+        </div>
+      </div>
+      <!-- 分项进度 -->
+      <div class="inc">
+        <div class="tit">分项进度</div>
+        <div ref="scrollRef" class="wrapper">
+          <div class="list">
+            <div class="item flex_center" v-for="index in 3" :key="index">
+              <van-circle v-model="currentRate" :rate="30" layer-color="#dcd6e5" />
+              <p>打卡300个话题的口语练习</p>
+              <div class="btn">去完成</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-
+  data() {
+    return {
+      scroll: null,
+      currentRate: 50
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.scrollRef, {
+      scrollX: true,
+      scrollY: false,
+      click: true,
+      bounce: false
+    })
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.goal {
+  width: 100%;
+  overflow: hidden;
+  padding-top: 50px;
+}
 .content {
   background-color: #f8f8f8;
   padding: 12px;
@@ -71,7 +121,7 @@ export default {
     }
   }
 }
-.study  {
+.study {
   position: relative;
   &::after {
     position: absolute;
@@ -81,7 +131,6 @@ export default {
     width: 2px;
     height: 16px;
     background-color: #a6a6a6;
-
   }
   margin-top: 16px;
   .today {
@@ -93,6 +142,57 @@ export default {
       font-size: 23px;
       font-weight: 700;
       margin-bottom: 10px;
+    }
+  }
+}
+
+// 当前级别
+.current {
+  .title {
+    font-size: 17px;
+    font-weight: 700;
+    color: #222;
+    padding: 12px;
+  }
+  .item {
+    padding: 12px;
+    p {
+      margin: 16px 0;
+    }
+  }
+}
+
+.inc {
+  width: 100%;
+  overflow: scroll-x;
+  background-color: #f8f8f8;
+  padding: 12px;
+  .tit {
+    font-size: 18px;
+    padding: 16px 0;
+  }
+  .list {
+    display: flex;
+    width: 200%;
+    // white-space: nowrap;
+    .item {
+      flex-direction: column;
+      float: left;
+      width: 220px;
+      background-color: #fff;
+      border-radius: 10px;
+      height: 220px;
+      margin-right: 12px;
+      p {
+        font-size: 13px;
+        margin: 16px 0;
+      }
+      .btn {
+        padding: 6px 16px;
+        color: #fff;
+        background-color: #eb4724;
+        border-radius: 40px;
+      }
     }
   }
 }
